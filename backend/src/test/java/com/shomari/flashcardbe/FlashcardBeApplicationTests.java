9f54abfdbc338e2651c7b8bc3111198e8609c9cb
Shomari Joseph <54215901+Shbucket@users.noreply.github.com>
2025-11-28 16:38:32 -0500

    created front end skeleton

---
+++ backend/src/test/java/com/shomari/flashcardbe/FlashcardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shomari.flashcardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
