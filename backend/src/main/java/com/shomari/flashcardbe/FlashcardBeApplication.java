c9d63858135120369d9876b3f7ebe4dc595e254f
Shomari Joseph <54215901+Shbucket@users.noreply.github.com>
2025-11-28 16:38:32 -0500

    created front end & back end skeleton

---
+++ backend/src/main/java/com/shomari/flashcardbe/FlashcardBeApplication.java
@@ -0,0 +1,13 @@
+package com.shomari.flashcardbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardBeApplication.class, args);
+	}
+
+}
