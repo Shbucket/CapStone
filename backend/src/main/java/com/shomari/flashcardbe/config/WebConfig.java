cf94ca697f8eb395e89b135dc8534578e84da06a
Shomari Joseph <54215901+Shbucket@users.noreply.github.com>
2025-12-07 18:46:10 -0500

    added front end to allowed origins changes to allow connection to it

--- backend/src/main/java/com/shomari/flashcardbe/config/WebConfig.java
+++ backend/src/main/java/com/shomari/flashcardbe/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("http://localhost:3000") // your frontend
+                        .allowedOrigins("https://cap-stone-flame.vercel.app/") // your frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
